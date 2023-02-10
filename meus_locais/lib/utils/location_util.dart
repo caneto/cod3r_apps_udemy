7b5b5880ead18f702c36e6eb6d24dc60e61d2402
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-02-10 16:07:31 -0300

    Update location_util.dart

--- meus_locais/lib/utils/location_util.dart
+++ meus_locais/lib/utils/location_util.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 
-const googleApiKey = 'AIzaSyB0thUKlszT06ocIXadlKVb4_Tp1Z_W9L8';
+const googleApiKey = ''; // Api fora do app
 
 class LocationUtil {
   static String generateLocationPreviewImage({
